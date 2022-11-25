6c7324723a8ed4623684d544adbb1479e10adf5d
DennisPynaerts <96107261+DennisPynaerts@users.noreply.github.com>
2022-11-25 15:04:25 +0100

    artefact

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const fs = require("fs");
 
 describe('get campus', () => {
     it('should return first campus', async() => {
@@ -7,7 +8,13 @@ describe('get campus', () => {
             .expect(200)
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect((res) => {
-                console.log(res.body[0])
+                fs.writeFile("output.json", JSON.stringify(res.body[0]), 'utf8', function(err) {
+                    if (err) {
+                        console.log("An error occured while writing JSON object to file.");
+                        return console.log(err);
+                    }
+                    console.log("JSON file has been saved");
+                });
             })
     });
 });
